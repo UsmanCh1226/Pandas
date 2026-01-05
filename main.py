23befa3fb2aa3ac4122e859b81d20a2ae77f73d4
UsmanCh1226 <usmanch@umich.edu>
2026-01-05 13:54:08 -0500

    Selecting

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 import pandas as pd
+
+df = pd.read_csv("learning_pandas.csv", index_col="Name")
+
+# SELECTION BY COLUMN
+#print(df["Name"].to_string())
+#print(df["Height"].to_string()) 
+#print
+#print(df[["Name", "Height", "Weight"]].to_string())
+
+#SELECTION BY ROWS 
+print(df.loc["Charizard": "Blastoise", ["Height", "Weight"]]) 
  
-data = {"Name": ["SpongeBob", "Patrick", "Squidward"],
-        "Age": [30, 35, 50]}
-
-df = pd.DataFrame(data)
- 
-
-#Add a new column
-df["Job"] = ["Cook ", "NA", "Cashier"]
-
-# Add a new row 
-new_rows = pd.DataFrame([{"Name": "Sandy", "Age": 28, "Job": "Engineer"},f
-                        {"Name": "Eugene", "Age": 60, "Job": "Manager" }],
-                       index= ["Employee 4", "Employee 5"])
-df = pd.concat([df, new_rows])
-
-
-print(df)
