9b6a3b64068122d466d386595edd5efa0130229c
UsmanCh1226 <usmanch@umich.edu>
2026-01-05 20:31:06 -0500

    Filtering

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import pandas as pd
 
-df = pd.read_csv("learning_pandas.csv", index_col="Name")
+df = pd.read_csv("learning_pandas.csv")
 
-# SELECTION BY COLUMN
-#print(df["Name"].to_string())
-#print(df["Height"].to_string()) 
-#print
-#print(df[["Name", "Height", "Weight"]].to_string())
+ # tall_pokemon = df[df["Height"] >= 2]
+#heavy_pokemon = df[df["Weight"] > 100]
+#legendary_pokemon = df[df["Legendary"] == 1]
+#water_pokemon = df[(df["Type1"] == "Water") | 
+#                   (df["Type2"] == "Water")]
 
-#SELECTION BY ROWS 
-print(df.loc["Charizard": "Blastoise", ["Height", "Weight"]]) 
+ff_pokemon = df[df["Type1"] == "Fire"]
  
+print(ff_pokemon)
+
+
+
